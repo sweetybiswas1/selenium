5a992ad055ccefff6aea3e9a9c5f0d553399adc5
sweetybiswas1 <sweety.biswas@cbnits.com>
2022-07-19 14:45:26 +0530

    check

---
+++ sel1.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_driver_path = "/home/cbnits/Downloads/chromedriver_linux64/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get("https://www.amazon.com")
+
+
+print(driver.title) #Returns Title of the page
+
+print(driver.current_url) #returns the url of the page
+
+#print(driver.page_source) # return the html code for the page
+
+driver.find_element(By.XPATH,'//*[@id="nav-signin-tooltip"]/a/span').click()
+
+time.sleep(10)
+
+#driver.close() #close the browser(close 1 browser at a time)(close currently foccused browser)
+
+#driver.quit() # closes all the browser
